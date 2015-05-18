93e7fc8f9def8d1fdc6e1388a09697890a4b72d5
alfonsonv <alfonso.nv@gmail.com>
2015-05-18 14:17:52 -0700

    issue #2

--- include/html/pg_clientes.php
+++ include/html/pg_clientes.php
@@ -1,4 +1,3 @@
-
 <p class="title">Clientes &raquo; Listado de clientes</p>
 <table>
 <caption>Listado completo de clientes</caption>
@@ -20,11 +19,11 @@
 	{
 		?>
 		<tr>
-			<th style="text-transform: uppercase;"><?echo $ln->nombre." ".$ln->apellidop." ".$ln->apellidom;?></th>
-			<td width="80"><a href="?pg=2e&cl=<?echo $ln->id;?>" class="tboton sombra esqRedondas cuenta">Cuenta</a></td>
-			<td width="80"><a href="include/html/box_cliente.php?width=700&height=530&cl=<?echo $ln->id;?>" class="thickbox tboton sombra esqRedondas detalles" title="Detalles de cliente">Detalles</a></td>
-			<td width="80"><a href="?pg=2b&cl=<?echo $ln->id;?>" rel="<?echo $ln->id;?>" class="tboton sombra esqRedondas editar">Editar</a></td>
-			<td width="80"><a href="include/html/box_cliente_elim.php?width=480&height=250&cl=<?echo $ln->id;?>" class="thickbox tboton sombra esqRedondas eliminar" title="Eliminar cliente">Eliminar</a></td>
+			<th style="text-transform: uppercase;"><?= $ln->nombre." ".$ln->apellidop." ".$ln->apellidom;?></th>
+			<td width="80"><a href="?pg=2e&cl=<?= $ln->id;?>" class="tboton sombra esqRedondas cuenta">Cuenta</a></td>
+			<td width="80"><a href="include/html/box_cliente.php?width=700&height=530&cl=<?= $ln->id;?>" class="thickbox tboton sombra esqRedondas detalles" title="Detalles de cliente">Detalles</a></td>
+			<td width="80"><a href="?pg=2b&cl=<?= $ln->id;?>" rel="<?= $ln->id;?>" class="tboton sombra esqRedondas editar">Editar</a></td>
+			<td width="80"><a href="include/html/box_cliente_elim.php?width=480&height=250&cl=<?= $ln->id;?>" class="thickbox tboton sombra esqRedondas eliminar" title="Eliminar cliente">Eliminar</a></td>
 		</tr>
 		<?php
 	}
