193e71281c2972c38a00207ea10040aced5f96c2
alfonsonv <alfonso.nv@gmail.com>
2015-05-18 13:13:24 -0700

    issue #4

--- include/html/pg_clientes_activos.php
+++ include/html/pg_clientes_activos.php
@@ -1,5 +1,5 @@
 <?php
-	session_start();
+	@session_start();
 	$UserName = $_SESSION["USERNAME"];
 	//echo $UserName;
 ?>
@@ -19,7 +19,7 @@
 	require_once("include/php/sys_db.class.php");
 	require_once("include/conf/Config_con.php");
 	$db = new DB(DB_DATABASE, DB_HOST, DB_USER, DB_PASSWORD);
-	$sql = "SELECT clientes.id, clientes.nombre, clientes.apellidop, clientes.apellidom, cuentas.cantidad, cuentas.cobrador 
+	$sql = "SELECT clientes.id, clientes.nombre, clientes.apellidop, clientes.apellidom, cuentas.total, cuentas.cobrador 
 	FROM clientes, cuentas 
 	WHERE clientes.id = cuentas.cliente 
 	AND cuentas.estado = 0 
@@ -38,12 +38,12 @@
 	while($r = $db->fetchNextObject($res)){
 		?>
 		<tr>
-			<td style="text-align:center"> <?echo $r->id;?></td>
-			<td style="text-align:center"><?echo $r->nombre . " ". $r->apellidop ." " .$r->apellidom;?></td>
-			<td style="text-align:center">$ <?echo $r->cantidad;?></td>
-			<td width="80"><a href="?pg=2e&cl=<?echo $r->id;?>" class="tboton sombra esqRedondas cuenta">Cuenta</a></td>
+			<td style="text-align:center"><?= $r->id;?></td>
+			<td style="text-align:center"><?= $r->nombre . " ". $r->apellidop ." " .$r->apellidom;?></td>
+			<td style="text-align:center">$ <?= $r->total;?></td>
+			<td width="80"><a href="?pg=2e&cl=<?= $r->id;?>" class="tboton sombra esqRedondas cuenta">Cuenta</a></td>
 		</tr>
-		<?
+		<?php
 	}
 	?>
 </tbody>
