03d8f663602f34aff68cdd086a3597675e3a8419
juanmarin <jmarincastro@gmail.com>
2015-05-19 23:00:56 -0700

    Se agrega restriccion para cobradores

--- include/html/pg_clientes_morosos.php
+++ include/html/pg_clientes_morosos.php
@@ -1,8 +1,7 @@
 <?php
 	session_start();
 	$UserName = $_SESSION["USERNAME"];
-	//echo "Nivel ".($_SESSION["U_NIVEL"])."<br/>";
-	//echo $UserName;
+	$UserLevel = $_SESSION["U_NIVEL"];
 ?>
 <p class="title">Portada &raquo; Listado de Clientes Morosos</p>
 <table>
@@ -19,9 +18,11 @@
 <tbody>
 	<?php
     $fecha = date("Y-m-d");
+echo $fecha;
 	require_once("include/php/sys_db.class.php");
 	require_once("include/conf/Config_con.php");
 	$db = new DB(DB_DATABASE, DB_HOST, DB_USER, DB_PASSWORD);
+	if($UserLevel == 0){
 	$sql = "SELECT clientes.id, clientes.nombre, clientes.apellidop, clientes.apellidop, clientes.demanda, cuentas.cliente, 
 				   cuentas.cobrador, cuentas.estado, pagos.cuenta, pagos.cliente, pagos.fecha, 
 				   SUM(pagos.pago) AS pago, pagos.estado
@@ -35,6 +36,24 @@
                 pagos.fecha < '".$fecha."'
             GROUP BY pagos.cliente 
             ORDER BY clientes.nombre ASC";
+	}elseif($UserLevel != 0){
+	$sql = "SELECT clientes.id, clientes.nombre, clientes.apellidop, clientes.apellidop, clientes.demanda, cuentas.cliente, 
+				   cuentas.cobrador, cuentas.estado, pagos.cuenta, pagos.cliente, pagos.fecha, 
+				   SUM(pagos.pago) AS pago, pagos.estado
+            FROM clientes, cuentas, pagos 
+            WHERE
+                clientes.id = cuentas.cliente AND
+		clientes.c_cobrador = '".$UserName."' AND
+                clientes.demanda != 1 AND
+                cuentas.id = pagos.cuenta AND
+		cuentas.cobrador = '".$UserName."' AND
+                cuentas.estado = 0 AND
+                pagos.estado = 0 AND
+                pagos.fecha < '".$fecha."'
+            GROUP BY pagos.cliente 
+            ORDER BY clientes.nombre ASC";
+	}{
+	}	
 	$res = $db->query($sql);
 	$num_rows = mysql_num_rows($res);
 	while($r = $db->fetchNextObject($res)){
