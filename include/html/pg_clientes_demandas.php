0eaef781e368ab5f37e5cc930957a1af1aa51510
juanmarin <jmarincastro@gmail.com>
2015-05-19 22:59:48 -0700

    Se agrega restriccion para cobradores

--- include/html/pg_clientes_demandas.php
+++ include/html/pg_clientes_demandas.php
@@ -1,3 +1,8 @@
+<?php
+	session_start();
+	$UserName = $_SESSION["USERNAME"];
+	$UserLevel = $_SESSION["U_NIVEL"];
+?>
 <p class="title">Portada &raquo; Listado de Clientes en Demanda</p>
 <table>
 <caption>CUENTAS EN PROCESO DE DEMANDA</caption>
@@ -14,11 +19,21 @@
 	require_once("include/php/sys_db.class.php");
 	require_once("include/conf/Config_con.php");
 	$db = new DB(DB_DATABASE, DB_HOST, DB_USER, DB_PASSWORD);
+	if($UserLevel == 0){
 	$sql = "SELECT clientes.id, clientes.nombre, clientes.apellidop, clientes.apellidop, demandas.cliente_id AS id 
             FROM clientes, demandas
             WHERE
                 clientes.id = demandas.cliente_id
             ORDER BY clientes.nombre ASC";
+	}elseif($UserLevel != 0){
+	$sql = "SELECT clientes.id, clientes.nombre, clientes.apellidop, clientes.apellidop, clientes.c_cobrador, demandas.cliente_id AS id 
+            FROM clientes, demandas
+            WHERE
+                clientes.id = demandas.cliente_id
+		AND clientes.c_cobrador = '".$UserName."'
+            ORDER BY clientes.nombre ASC";
+	} {
+	}
 	$res = $db->query($sql);
 	$num_rows = mysql_num_rows($res);
 	while($r = $db->fetchNextObject($res)){
@@ -27,19 +42,14 @@
 			<td style="text-align:center"> <?php echo $r->id;?></td>	
 			<td style="text-align:center"><?php echo $r->nombre ." ". $r->apellidop ." " .$r->apellidom;?></td>
 			<td width="80"><a href="?pg=2e&cl=<?php echo $r->id;?>" class="tboton sombra esqRedondas cuenta">Cuenta</a></td>
-			<!--
-<td width="80"><a href="include/php/sys_modelo.php?cte=<?echo $r->id;?>&cta=<?echo $r->cuenta;?>&action=recargo_elimina" class="tboton sombra esqRedondas recargos">Elimina</a></td>
--->
-
 		</tr>
-		<?php
+	<?php
 	}
 	?>
 </tbody>
 <tfoot>
 	<tr>
-		<th colspan="5">
-			</th>
+		<th colspan="5"></th>
 	</tr>
 </tfoot>
 </table>
