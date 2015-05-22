2be171b273089c8a4dd2420d84525a05b58ecfae
juanmarin <jmarincastro@gmail.com>
2015-05-21 21:16:07 -0700

    Se corrigio la extencion del archivo que se genera xsl por xls

--- xls.php
+++ xls.php
@@ -3,7 +3,7 @@
 if(isset($_SESSION["U_NIVEL"]) && $_SESSION["U_NIVEL"]==0 && isset($_POST)){
 	header("Content-Type:   application/vnd.ms-excel; charset=utf-8");
 	header("Content-type:   application/x-msexcel; charset=utf-8");
-	header("Content-Disposition: attachment; filename=abc.xsl"); 
+	header("Content-Disposition: attachment; filename=abc.xls"); 
 	header("Expires: 0");
 	header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
 	header("Cache-Control: private",false);
