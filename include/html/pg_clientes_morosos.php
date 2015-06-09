393ea5debc40e09d5a247befeaaed84253739136
juanmarin <jmarincastro@gmail.com>
2015-06-08 22:29:56 -0700

    Se agrego filtro de cobradores a listado de clientes morosos

--- include/html/pg_clientes_morosos.php
+++ include/html/pg_clientes_morosos.php
@@ -2,14 +2,34 @@
 @session_start();
 $UserName = $_SESSION["USERNAME"];
 $UserLevel = $_SESSION["U_NIVEL"];
-?>
-<p class="title">Portada &raquo; Listado de Clientes Morosos</p>
+    $fecha = date("Y-m-d");
+	require_once("include/php/sys_db.class.php");
+	require_once("include/conf/Config_con.php");
+	$db = new DB(DB_DATABASE, DB_HOST, DB_USER, DB_PASSWORD);
+	$UserName = $_SESSION["USERNAME"];
+	$UserLevel = $_SESSION["U_NIVEL"];
+if($UserLevel==0){
+	if(isset($_POST["cobrador"])){
+		if($_POST["cobrador"]=="0"){
+			$clcobrador="";
+		}else{
+			$clcobrador="AND clientes.c_cobrador = '".$_POST["cobrador"]."'";
+		}
+	}else{
+		$clcobrador="";
+	}
+}else{
+	$clcobrador="AND clientes.c_cobrador = '$UserName'";
+}if (isset($_POST["enviar"])){
+	?>
+	<p class="title">Portada &raquo; Listado de Clientes Morosos</p>
 <table>
 <caption>CUENTAS CON RECARGOS</caption>
 <thead>
 	<tr>
 		<th>ID</th>
 		<th>CLIENTE</th>
+		<th>COBRADOR</th>
 		<th>CARGOS</th>
 		<th>DEMANDAR</th>
 		<th colspan="2">ACCIONES</th>
@@ -17,16 +37,7 @@ $UserLevel = $_SESSION["U_NIVEL"];
 </thead>
 <tbody>
 	<?php
-    $fecha = date("Y-m-d");
-	require_once("include/php/sys_db.class.php");
-	require_once("include/conf/Config_con.php");
-	$db = new DB(DB_DATABASE, DB_HOST, DB_USER, DB_PASSWORD);
-	if( $UserLevel == 0 ){
-		$cobrador = "";
-	} else {
-		$cobrador = "AND c_cobrador = '$UserName'";
-	}
-	$sql = "SELECT clientes.id, clientes.nombre, clientes.apellidop, clientes.apellidop, clientes.demanda, cuentas.cliente, 
+	$sql = "SELECT clientes.id, clientes.nombre, clientes.apellidop, clientes.apellidop, clientes.demanda, cuentas.cliente, clientes.c_cobrador, 
 	cuentas.cobrador, cuentas.estado, pagos.cuenta, pagos.cliente, pagos.fecha, 
 	SUM(pagos.pago) AS pago, pagos.estado
 	FROM clientes, cuentas, pagos 
@@ -37,7 +48,7 @@ $UserLevel = $_SESSION["U_NIVEL"];
 		AND cuentas.estado = 0 
 		AND pagos.estado = 0 
 		AND pagos.fecha < '".$fecha."'
-		$cobrador
+		$clcobrador
 	GROUP BY pagos.cliente 
 	ORDER BY clientes.nombre ASC";
 	$res = $db->query($sql);
@@ -49,6 +60,7 @@ $UserLevel = $_SESSION["U_NIVEL"];
 		<tr>
 			<td style="text-align:center"> <?= $r->id;?></td>	
 			<td style="text-align:center"><?= $r->nombre ." ". $r->apellidop ." " .$r->apellidom;?></td>
+			<td style="text-align:center"> <?= $r->c_cobrador;?></td>
 			<td style="text-align:center">$ <?= moneda($r->pago);?></td>
 			<th style="text-align:center"><input type="checkbox" name="ids[]" value="<?= $r->id; ?>" /></th>
 			<td width="80"><a href="?pg=2e&cl=<?= $r->id;?>" class="tboton sombra esqRedondas cuenta">Cuenta</a></td>
@@ -59,10 +71,55 @@ $UserLevel = $_SESSION["U_NIVEL"];
 </tbody>
 <tfoot>
 	<tr>
-		<th colspan="5">
+		<th colspan="6">
 			<input type="submit" name="demanda" id="demanda" value="Guardar &raquo;" />
 			</th>
 	</tr>
-</tfoot>
-</form>
-</table>
+	</tfoot>
+	</form>
+	</table>
+	<?php
+}else{
+	?>
+	<p class="title">Clientes &raquo; Vencidos</p>
+	<form name="repoFechas" action="" method="post">
+	<table>
+	<caption>Clientes con Recargos</caption>
+	<thead>
+	<tr>
+	<th colspan="4"></th>
+	</tr>
+	</thead>
+	<tbody>
+	<?php
+	if($UserLevel==0){
+		?>
+		<tr>
+		<th colspan="2">Seleccione un cobrador:</th>
+		<td colspan="2">
+		<select name="cobrador" id="cobrador">
+		<option value="0">Todos</option>
+		<?php
+		$sql = "SELECT username FROM mymvcdb_users WHERE nivel=3";
+		$res = $db->query($sql);
+		while( $cob = $db->fetchNextObject($res) ){
+			echo '<option value="'.$cob->username.'">'.$cob->username.'</option>';
+		}
+		?>
+		</select>
+		</td>
+		</tr>
+		<?php
+	}
+	?>
+	</tbody>
+	<tfoot>
+	<tr>
+	<th colspan="4"><input type="submit" value="Mostrar Reporte" name="enviar" /></th>
+	</tr>
+	</tfoot>
+	</table>
+	</form>
+	<?php
+}
+?>
