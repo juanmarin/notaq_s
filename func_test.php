3924bbb66e818d23343b78da6a04985d16d22ae2
alfonsonv <alfonso.nv@gmail.com>
2015-07-04 18:08:04 -0700

    Reporte de vendedores

--- func_test.php
+++ func_test.php
@@ -7,6 +7,7 @@
 		$plazo2 = 5;
 		$monto2 = 100;
 		$tipo_pago = "SEMANAL";
+		$hoy = date("Y-m-d");
 		
 	$datosPrestamo = calculamonto($cantidad, $monto1, $monto2, $plazo1, $plazo2, $tipo_pago);
 	
@@ -38,15 +39,24 @@
 		require_once("include/php/sys_db.class.php");
 		require_once("include/php/fun_global.php");
 		require_once("include/conf/Config_con.php");
-			$db = new DB(DB_DATABASE, DB_HOST, DB_USER, DB_PASSWORD);
-			/*
-		    $sql = "SELECT mymvcdb_users.username AS cobrador, COUNT(clientes.c_cobrador) AS mis_ctes
-					FROM mymvcdb_users,clientes 
-					WHERE mymvcdb_users.username = clientes.c_cobrador
-					AND clientes.activo = 1
-					GROUP BY clientes.c_cobrador";
-			$res = $db->query($sql);
-			#Buscando el total de clientes Morosos
+		$db = new DB(DB_DATABASE, DB_HOST, DB_USER, DB_PASSWORD);
+		
+		$sql = "SELECT mymvcdb_users.username AS cobrador, COUNT(clientes.c_cobrador) AS mis_ctes
+			FROM mymvcdb_users,clientes 
+			WHERE mymvcdb_users.username = clientes.c_cobrador
+			AND clientes.activo = 1
+			GROUP BY clientes.c_cobrador";
+		$res = $db->query($sql);
+		while($est=$db->fetchNextObject($sql))
+		{
+			?>
+			<tr> 
+		    		<td><?php echo $cob->cobrador;?></td>
+		    		<td> <?php echo $cob->mis_ctes; ?></td>
+				<td><!-- C. Corriente --></td>
+				<?php
+				#Buscando el total de clientes Morosos
+				/*
 				$sql2 = "SELECT mymvcdb_users.username, clientes.id, clientes.nombre, clientes.apellidop, clientes.apellidop, 
 				cuentas.cliente, clientes.c_cobrador, cuentas.cobrador, cuentas.estado, pagos.cuenta, pagos.cliente, pagos.fecha, 
 				SUM(pagos.pago) AS pago, pagos.estado
@@ -58,29 +68,35 @@
 					AND cuentas.estado = 0 
 					AND pagos.estado = 0 
 					AND pagos.fecha < '".$fecha."'";
-				$res = $db->query($sql2);
-				while ($cob = $db->fetchNextObject($res)) {
-					*/
-		    	?>
-		    	 	<tr> 
-		    			<td><?php echo $cob->cobrador;?></td>
-		    			<td> <?php echo $cob->mis_ctes; ?></td>
-		    		</tr>
+				*/
+				$sql2=" SELECT count(*) morosos FROM cuentas c LEFT JOIN pagos p ON c.id=p.id 
+					WHERE c.estado = 0 AND c.cobrador='".$cob->cobrador."' AND p.estado=0 AND p.fecha <= '$hoy'";
+				$res2 = $db->query($sql2);
+				while ($mor = $db->fetchNextObject($res2))
+				{
+					?>
+					<td><?=$mor->morosos;?></td>
+					<?php
+				}
+				?>
+				<td><!-- Total avance % --></td>
+			</tr>
 		    	<?php
-		    	
-		    //}
-		  
-		    	echo $cob->cobrador."----->".$cob->mis_ctes."</br>";
-
-
-		    $cuenta = 144;
-		    $cliente = 120;/*
+		}
+?>
+</tbody>
+</table>
+<?php
+/*
+		echo $cob->cobrador."----->".$cob->mis_ctes."</br>";
+		$cuenta = 144;
+		$cliente = 120;/*
 		    if (hayRecargos($cuenta, $cliente) == 1) {
 		    	echo "tiene Recargos";
 		    }
 
 				*/
-				
+				/*
 		    $sql = "SELECT COUNT(clientes.c_cobrador) AS cobradores, mymvcdb_users.nombre, mymvcdb_users.username, count( clientes.id )
 				FROM `clientes`
 				JOIN mymvcdb_users ON mymvcdb_users.username = clientes.c_cobrador
