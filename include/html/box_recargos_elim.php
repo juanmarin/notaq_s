76cb956071b95f6bdb05ad1c23d63e33a562d787
juanmarin <jmarincastro@gmail.com>
2015-08-09 15:24:53 -0700

    Se agrega tickbox para condonar recargos y cambios menores a cuenta_cliente

--- include/html/box_recargos_elim.php
+++ include/html/box_recargos_elim.php
@@ -6,14 +6,13 @@
 </style>
 <form action="include/php/sys_modelo.php" method="post">
 <table>
-<caption>Cancelar Pagos</caption>
+<caption>Condonar Recargos</caption>
 <thead>
 	<tr>
-		<th align="center"># Pago</th>
+		<th align="center">#</th>
 		<th align="center">Fecha</th>
-		<th align="center">F.Pago</th>
-		<th align="center">Monto</th>	
-		<th align="center">Aplicado</th>
+		<th align="center">D. Venc.</th>
+		<th align="center">Monto</th>
 		<th align="center">Acciones</th>
 	</tr>
 </thead>
@@ -22,17 +21,18 @@
 	require_once("../php/sys_db.class.php");
 	require_once("../conf/Config_con.php");
 	$db = new DB(DB_DATABASE, DB_HOST, DB_USER, DB_PASSWORD);
-	$result = $db->query("SELECT * FROM pagos WHERE estado = 1 AND cuenta = ".$_GET["cta"]);
+	$result = $db->query("SELECT * FROM recargos WHERE estado = 0 AND cuenta = ".$_GET["cta"]);
+	echo $db->query;
 	$i = 0;
 	while ($ln = $db->fetchNextObject($result)){
 		$i++;
 ?>
+
 	<tr>
 		<td align="center"><?php echo $i; ?></td>
-		<td align="center"><?php echo date("d-m-Y", strtotime($ln->fecha)); ?></td>
-        <td align="center"><?php echo date("d-m-Y", strtotime($ln->fechaPago)); ?></td>
-        <td align="center"><?php echo "$".number_format($ln->pago, 2); ?></td>
-        <td align="center"><?php echo $ln->aplicado_x; ?></td>
+		<td align="center"><?php echo date("d-m-Y", strtotime($ln->pago)); ?></td>
+        <td align="center"><?php echo $ln->dias_atraso; ?></td>
+        <td align="center"><?php echo "$".number_format($ln->monto, 2); ?></td>
         <td align="center"><input type="checkbox" name="ids[]" value="<?= $ln->id; ?>"></td>
 	</tr>
 	<?php
@@ -47,7 +47,7 @@
 		<input type="hidden" name="cta" value="<?php echo $_GET['cta']; ?>" />
 		<input type="hidden" name="pago" value="<?php echo $ln->pago; ?>" />
 		<input type="hidden" name="action" value="pago_elimina" />
-		<input type="submit" name="eliminar_cuenta" value="Cancelar Pago(s)" />
+		<input type="submit" name="eliminar_cuenta" value="Condonar Recargo(s)" />
 		</th>
 	</tr>				
 </tfoot>
