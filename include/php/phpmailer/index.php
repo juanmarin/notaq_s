999b7fda673edbf59e37402b7224b8b40aee3f4f
juanmarin <jmarincastro@gmail.com>
2015-09-08 09:54:01 -0700

    Se agega clase para envio de correos con attachments

---
+++ include/php/phpmailer/index.php
@@ -0,0 +1,46 @@
+<?php
+require("class.phpmailer.php");
+require("class.smtp.php");
+/*
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = "ssl";
+$mail->Host = "smtp.gmail.com";
+$mail->Port = 465;
+$mail->Username = "jmarincastro34@gmail.com";
+$mail->Password = "kebbduzer";
+
+$mail->From = "jmarincastro34@gmail.com";
+	$mail->FromName = "Juan Mairin";
+	$mail->Subject = "Enviando Mail con PHPMailer";
+	$mail->AltBody = "";
+	$mail->MsgHTML("<h1>Test mail message!</h1>");
+$mail->AddAttachment("adjunto.txt");
+
+$mail->AddAddress("jmarincastro@hotmail.com", "Nombre Destinatario");
+$mail->IsHTML(true);
+$mail->Send();
+*/
+///// SIN GMAIL.COM ////
+
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->SMTPAuth = true;
+//$mail->SMTPSecure = "ssl";
+$mail->Host = "mail.confianzp.com";
+$mail->Port = 25;
+$mail->Username = "juan.marin@confianzp.com";
+$mail->Password = "V1ct0r1a";
+
+$mail->From = "juan.marin@confianzp.com";
+	$mail->FromName = "Juan Mairin";
+	$mail->Subject = "Enviar Mail con PHPMailer desde local";
+	$mail->AltBody = "";
+	$mail->MsgHTML("<h1>Test Mail Messages Stage Segundo intento!</h1>");
+$mail->AddAttachment("adjunto.txt");
+
+$mail->AddAddress("jmarincastro@gmail.com", "Juan Mairin");
+$mail->IsHTML(true);
+$mail->Send();
+?>
