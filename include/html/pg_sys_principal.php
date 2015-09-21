98250fda80283e6b5f7f9a0902cdb3cbfbc83f90
alfonsonv <alfonso.nv@gmail.com>
2015-09-20 19:39:23 -0700

    Generar tabla de desempeño por semana

--- include/html/pg_sys_principal.php
+++ include/html/pg_sys_principal.php
@@ -7,76 +7,73 @@ $UserLevel = $_SESSION["U_NIVEL"];
 <?php
 if ($UserLevel == 0) {
 	$clcobrador = "";
-?>
-<table>
-<caption>REPORTE DE COBRADORES</caption>
-<thead>
-	<tr>
-		<th>COBRADOR</th>
-		<th>USUARIO</th>
-		<th>ASIG</th>
-		<th>CTE</th>
-		<th>VENC</th>
-		<th>TOTAL %</th>
-	</tr>
-</thead>
-<tbody>
-<?php
-		//// ESTADISTICOS DE COBRADORES///
-		require_once("include/php/sys_db.class.php");
-		require_once("include/php/fun_global.php");
-		require_once("include/conf/Config_con.php");
-		$fecha = date("Y-m-d");
-		$db = new DB(DB_DATABASE, DB_HOST, DB_USER, DB_PASSWORD);
-		$sql = "SELECT mymvcdb_users.username AS cobrador, mymvcdb_users.nombre, clientes.id, COUNT(clientes.c_cobrador) AS mis_ctes, cuentas.cliente, cuentas.estado
-			FROM mymvcdb_users,clientes,cuentas
-			WHERE mymvcdb_users.username = clientes.c_cobrador
-			AND clientes.id = cuentas.cliente
-			AND cuentas.estado = 0
-			GROUP BY mymvcdb_users.username";
-		$res = $db->query($sql);
-		while($cob=$db->fetchNextObject($res))
-		{
+	?>
+	<table>
+	<caption>REPORTE DE COBRADORES</caption>
+	<thead>
+		<tr>
+			<th>COBRADOR</th>
+			<th>USUARIO</th>
+			<th>ASIG</th>
+			<th>CTE</th>
+			<th>VENC</th>
+			<th>TOTAL %</th>
+		</tr>
+	</thead>
+	<tbody>
+	<?php
+	//// ESTADISTICOS DE COBRADORES///
+	require_once("include/php/sys_db.class.php");
+	require_once("include/php/fun_global.php");
+	require_once("include/conf/Config_con.php");
+	$fecha = date("Y-m-d");
+	$db = new DB(DB_DATABASE, DB_HOST, DB_USER, DB_PASSWORD);
+	$sql = "SELECT mymvcdb_users.username AS cobrador, mymvcdb_users.nombre, clientes.id, COUNT(clientes.c_cobrador) AS mis_ctes, cuentas.cliente, cuentas.estado
+		FROM mymvcdb_users,clientes,cuentas
+		WHERE mymvcdb_users.username = clientes.c_cobrador
+		AND clientes.id = cuentas.cliente
+		AND cuentas.estado = 0
+		GROUP BY mymvcdb_users.username";
+	$res = $db->query($sql);
+	while($cob=$db->fetchNextObject($res))
+	{
+		?>
+		<tr>
+			<td style="font-size:small;"><?php echo $cob->nombre;?></td>
+			<td style="font-size:small;"><?php echo $cob->cobrador;?></td>
+			<td style="font-size:small;" align="center"> <?php echo $cob->mis_ctes; ?></td>
+			<?php
+				$sql2="SELECT clientes.id, clientes.demanda, cuentas.cliente, clientes.c_cobrador, 
+			cuentas.cobrador, cuentas.estado, 
+			pagos.cuenta, pagos.cliente, pagos.fecha, pagos.estado
+			FROM clientes, cuentas, pagos 
+			WHERE
+				clientes.id = cuentas.cliente 
+				AND clientes.demanda != 1 
+				AND cuentas.id = pagos.cuenta 
+				AND cuentas.estado = 0 
+				AND pagos.estado = 0 
+				AND pagos.fecha < '".$fecha."'
+				AND clientes.c_cobrador='".$cob->cobrador."'
+				GROUP BY pagos.cliente";
+				$res2 = $db->query($sql2);
+				$morosos = mysql_num_rows($res2);
+				while ($mor = $db->fetchNextObject($res2))
+				{
+					$corriente=$cob->mis_ctes-$morosos;
+					$avance = ($corriente/$cob->mis_ctes)*100;
+				}
+				$tot_asi += $cob->mis_ctes;
+				$tot_corr += $corriente;
+				$tot_mor += $morosos;	
+				$tot_avance = ($tot_corr/$tot_asi)*100;
 			?>
-			<tr>
-				<td style="font-size:small;"><?php echo $cob->nombre;?></td>
-				<td style="font-size:small;"><?php echo $cob->cobrador;?></td>
-				<td style="font-size:small;" align="center"> <?php echo $cob->mis_ctes; ?></td>
-				<?php
-					$sql2="SELECT clientes.id, clientes.demanda, cuentas.cliente, clientes.c_cobrador, 
-				cuentas.cobrador, cuentas.estado, 
-				pagos.cuenta, pagos.cliente, pagos.fecha, pagos.estado
-				FROM clientes, cuentas, pagos 
-				WHERE
-					clientes.id = cuentas.cliente 
-					AND clientes.demanda != 1 
-					AND cuentas.id = pagos.cuenta 
-					AND cuentas.estado = 0 
-					AND pagos.estado = 0 
-					AND pagos.fecha < '".$fecha."'
-					AND clientes.c_cobrador='".$cob->cobrador."'
-					GROUP BY pagos.cliente";
-					$res2 = $db->query($sql2);
-					$morosos = mysql_num_rows($res2);
-					while ($mor = $db->fetchNextObject($res2))
-					{
-						$corriente=$cob->mis_ctes-$morosos;
-						$avance = ($corriente/$cob->mis_ctes)*100;
-					?>
-						
-					<?php
-					}
-					$tot_asi += $cob->mis_ctes;
-					$tot_corr += $corriente;
-					$tot_mor += $morosos;	
-					$tot_avance = ($tot_corr/$tot_asi)*100;
-				?>
-			<td style="background-color: #7DB77B; font-size:small" align="center"><?php echo $corriente;?></td>
-			<td style="background-color: #F78181; font-size:small" align="center"><?php echo $morosos;?></td>
-			<td style="font-size:small;" align="center"><?php echo number_format($avance, 2)."%";?></td>
-			</tr>
-		    	<?php
-		}
+		<td style="background-color: #7DB77B; font-size:small" align="center"><?php echo $corriente;?></td>
+		<td style="background-color: #F78181; font-size:small" align="center"><?php echo $morosos;?></td>
+		<td style="font-size:small;" align="center"><?php echo number_format($avance, 2)."%";?></td>
+		</tr>
+	    	<?php
+	}
 ?>
 	<tr style="font-weight: bold;">
 		<td colspan="2">Totales</td>
@@ -305,54 +302,52 @@ if(isset($_POST["desempxtiempo"]))
 }
 } else {
 	$clcobrador = "AND c_cobrador = '$UserName'";
-?>
-<table>
-<caption>COBRADOR: &nbsp; <b><?php echo $user->userData[6];?></b></caption>
-<thead>
-	<tr>
-		<th>C. ASIGNADOS</th>
-		<th>C. CORRIENTE</th>
-		<th>C. VENCIDOS</th>
-		<th>TOTAL AVANCE %</th>
-	</tr>
-</thead>
-<tbody>
+	?>
+	<table>
+	<caption>COBRADOR: &nbsp; <b><?php echo $user->userData[6];?></b></caption>
+	<thead>
+		<tr>
+			<th>C. ASIGNADOS</th>
+			<th>C. CORRIENTE</th>
+			<th>C. VENCIDOS</th>
+			<th>TOTAL AVANCE %</th>
+		</tr>
+	</thead>
+	<tbody>
 	<?php
 	$fecha = date("Y-m-d");
 		require_once("include/php/sys_db.class.php");
 		require_once("include/php/fun_global.php");
 		require_once("include/conf/Config_con.php");
-			$db = new DB(DB_DATABASE, DB_HOST, DB_USER, DB_PASSWORD);
+		$db = new DB(DB_DATABASE, DB_HOST, DB_USER, DB_PASSWORD);
 		#Buscando los clientes asignados al cobrador con cuentas abiertas
-			$sql = "SELECT clientes.id, clientes.activo, clientes.c_cobrador, cuentas.cliente, cuentas.estado 
-			FROM clientes, cuentas 
-			WHERE activo = 1
-			$clcobrador
-			AND clientes.id = cuentas.cliente 
-			AND cuentas.estado = 0";
-			$res = $db->query($sql);
-			$mis_ctes = mysql_num_rows($res);
-
+		$sql = "SELECT clientes.id, clientes.activo, clientes.c_cobrador, cuentas.cliente, cuentas.estado 
+		FROM clientes, cuentas 
+		WHERE activo = 1
+		$clcobrador
+		AND clientes.id = cuentas.cliente 
+		AND cuentas.estado = 0";
+		$res = $db->query($sql);
+		$mis_ctes = mysql_num_rows($res);
 		#Buscando el total de clientes Morosos
-			$sql = "SELECT clientes.id, clientes.demanda, cuentas.cliente, clientes.c_cobrador, 
-			cuentas.cobrador, cuentas.estado, 
-			pagos.cuenta, pagos.cliente, pagos.fecha, pagos.estado
-			FROM clientes, cuentas, pagos 
-			WHERE
-			clientes.id = cuentas.cliente 
-			AND clientes.demanda != 1 
-			AND cuentas.id = pagos.cuenta 
-			AND cuentas.estado = 0 
-			AND pagos.estado = 0 
-			AND pagos.fecha < '".$fecha."'
-				$clcobrador
-			GROUP BY pagos.cliente 
-			ORDER BY clientes.nombre ASC";
-			$res = $db->query($sql);
-			$mis_morosos = mysql_num_rows($res);
-			$mis_corriente = ($mis_ctes - $mis_morosos);
-			$avance = ($mis_corriente/$mis_ctes)*100;
-
+		$sql = "SELECT clientes.id, clientes.demanda, cuentas.cliente, clientes.c_cobrador, 
+		cuentas.cobrador, cuentas.estado, 
+		pagos.cuenta, pagos.cliente, pagos.fecha, pagos.estado
+		FROM clientes, cuentas, pagos 
+		WHERE
+		clientes.id = cuentas.cliente 
+		AND clientes.demanda != 1 
+		AND cuentas.id = pagos.cuenta 
+		AND cuentas.estado = 0 
+		AND pagos.estado = 0 
+		AND pagos.fecha < '".$fecha."'
+		$clcobrador
+		GROUP BY pagos.cliente 
+		ORDER BY clientes.nombre ASC";
+		$res = $db->query($sql);
+		$mis_morosos = mysql_num_rows($res);
+		$mis_corriente = ($mis_ctes - $mis_morosos);
+		$avance = ($mis_corriente/$mis_ctes)*100;
 		?>
 		<tr>
 			<td style="text-align:center"> <?php echo $mis_ctes;?></td>
@@ -360,68 +355,66 @@ if(isset($_POST["desempxtiempo"]))
 			<td style="background-color: #F78181;" align="center"><?php echo $mis_morosos;?></td>
 			<td align="right"><?php echo number_format($avance, 2)."%";?></td>
 		</tr>
-</tbody>
-<tfoot>
-	<tr>
-		<th colspan="4"></th>
-	</tr>
-</tfoot>
-</table>
-<br/>
-<br/>
-<table>
-<caption>COBROS PARA EL DIA :<b><?php echo date("d-m-Y", strtotime($fecha)); ?></b> </caption>
-<thead>
-	<tr>
-		<th>C. POR COBRAR</th>
-		<th>C. REALIZADOS</th>
-		<th>AVANCE DEL DIA %</th>
-	</tr>
-</thead>
-<tbody>
+	</tbody>
+	<tfoot>
+		<tr>
+			<th colspan="4"></th>
+		</tr>
+	</tfoot>
+	</table>
+	<br/>
+	<br/>
+	<table>
+	<caption>COBROS PARA EL DIA :<b><?php echo date("d-m-Y", strtotime($fecha)); ?></b> </caption>
+	<thead>
+		<tr>
+			<th>C. POR COBRAR</th>
+			<th>C. REALIZADOS</th>
+			<th>AVANCE DEL DIA %</th>
+		</tr>
+	</thead>
+	<tbody>
 	<?php
 		#Buscando los clientes asignados al cobrador
 		require_once("include/php/sys_db.class.php");
 		require_once("include/php/fun_global.php");
 		require_once("include/conf/Config_con.php");
-			$db = new DB(DB_DATABASE, DB_HOST, DB_USER, DB_PASSWORD);
+		$db = new DB(DB_DATABASE, DB_HOST, DB_USER, DB_PASSWORD);
 		#Buscando el total de clientes a visitar hoy
-			$sql = "SELECT clientes.id, clientes.c_cobrador, pagos.id, pagos.cliente, pagos.cuenta, pagos.fecha, pagos.estado 
-			FROM clientes, pagos 
-			WHERE clientes.id = pagos.cliente 
-			AND pagos.fecha = '".$fecha."' 
-			AND pagos.estado = 0
-			$clcobrador";
-			$res = $db->query($sql);
-			$x_visitar = mysql_num_rows($res);
+		$sql = "SELECT clientes.id, clientes.c_cobrador, pagos.id, pagos.cliente, pagos.cuenta, pagos.fecha, pagos.estado 
+		FROM clientes, pagos 
+		WHERE clientes.id = pagos.cliente 
+		AND pagos.fecha = '".$fecha."' 
+		AND pagos.estado = 0
+		$clcobrador";
+		$res = $db->query($sql);
+		$x_visitar = mysql_num_rows($res);
 		#Buscando el total de clientes a visitados hoy
-			$sql = "SELECT clientes.id, clientes.c_cobrador, pagos.id, pagos.cliente, pagos.cuenta, pagos.fecha, pagos.estado 
-			FROM clientes, pagos 
-			WHERE clientes.id = pagos.cliente 
-			AND pagos.fecha = '".$fecha."' 
-			AND pagos.fechaPago = '".$fecha."' 
-			AND pagos.estado = 1
-			$clcobrador";
-			$res = $db->query($sql);
-			$visitados = mysql_num_rows($res);
-			$avanced = ($visitados/($x_visitar+$visitados))*100;
-			$tbl_color = semaforo(number_format($avanced, 2));
-
-
+		$sql = "SELECT clientes.id, clientes.c_cobrador, pagos.id, pagos.cliente, pagos.cuenta, pagos.fecha, pagos.estado 
+		FROM clientes, pagos 
+		WHERE clientes.id = pagos.cliente 
+		AND pagos.fecha = '".$fecha."' 
+		AND pagos.fechaPago = '".$fecha."' 
+		AND pagos.estado = 1
+		$clcobrador";
+		$res = $db->query($sql);
+		$visitados = mysql_num_rows($res);
+		$avanced = ($visitados/($x_visitar+$visitados))*100;
+		$tbl_color = semaforo(number_format($avanced, 2));
 		?>
 		<tr style="font-weight: bold;">
 			<td style="text-align:center"> <?php echo $x_visitar;?></td>
 			<td style="background-color: "align="center"><?php echo $visitados;?></td>
 			<td style="background-color:<?php echo $tbl_color;?>" align="right"><?php echo number_format($avanced, 2)."%";?></td>
 		</tr>
-</tbody>
-<tfoot>
-	<tr>
-		<th colspan="4"></th>
-	</tr>
-</tfoot>
-</table>
-<br />
+	</tbody>
+	<tfoot>
+		<tr>
+			<th colspan="4"></th>
+		</tr>
+	</tfoot>
+	</table>
+	<br />
 	<script>
 	$("#filtromapas").change(function(){
 		$("#mapa").attr("src",$(this).val());
@@ -454,7 +447,7 @@ if(isset($_POST["desempxtiempo"]))
 		</tr>
 	</tbody>
 	<tfoot>
-		<tr><th></th></tr>
+		<tr><th>&nbsp;</th></tr>
 	</tfoot>
 	</table>
 <br/>
---
+++ include/php/set_desempeno.php
@@ -0,0 +1,112 @@
+<?php
+//-INICIALIZAR-TABLA-DESEMPEÑO/////////////////////////////////////////////////////////////////////
+/*-tabla desempeno--//
+CREATE TABLE `desempeno` (
+  `Iddes` int(11) NOT NULL auto_increment,
+  `semana` int(3) NOT NULL default '0',
+  `cobrador` varchar(255) NOT NULL default '',
+  `total` double default NULL,
+  `en_fecha` int(11) default NULL,
+  `fuera_fecha` int(11) default NULL,
+  `por_cobrar` int(11) default NULL,
+  PRIMARY KEY  (`Iddes`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*-----------------------------------------------trigger-
+DELIMITER //
+DROP TRIGGER IF EXISTS `updatedesempeno_pagos` //
+CREATE TRIGGER `updatedesempeno_pagos` 
+AFTER UPDATE ON `pagos`
+FOR EACH ROW
+BEGIN
+	IF OLD.estado = 0 AND NEW.estado > 0 THEN
+		IF NEW.fechaPago <= OLD.fecha THEN
+			UPDATE desempeno 
+			SET en_fecha = en_fecha + 1, por_cobrar = por_cobrar - 1
+			WHERE semana=WEEK(NOW(),3) AND cobrador=NEW.aplicado_x;
+		ELSEIF NEW.fechaPago > OLD.fecha THEN
+			UPDATE desempeno 
+			SET fuera_fecha = fuera_fecha + 1, por_cobrar = por_cobrar - 1
+			WHERE semana=WEEK(NOW(),3) AND cobrador=NEW.aplicado_x;
+		END IF;
+	ELSEIF OLD.estado > 0 AND NEW.estado = 0 THEN
+		IF OLD.fechaPago <= OLD.fecha THEN
+			UPDATE desempeno 
+			SET en_fecha = en_fecha - 1, por_cobrar = por_cobrar + 1
+			WHERE semana=WEEK(NOW(),3) AND cobrador=NEW.aplicado_x;
+		ELSEIF OLD.fechaPago > OLD.fecha THEN
+			UPDATE desempeno 
+			SET fuera_fecha = fuera_fecha - 1, por_cobrar = por_cobrar + 1
+			WHERE semana=WEEK(NOW(),3) AND cobrador=NEW.aplicado_x;
+		END IF;
+	END IF;
+END;//
+DELIMITER ;
+---------------------------------------------------------*/
+//////////////////////////////////////////////////////////////////////////////////////////////////
+require_once("sys_db.class.php");
+require_once("fun_global.php");
+require_once("../conf/Config_con.php");
+$db = new DB(DB_DATABASE, DB_HOST, DB_USER, DB_PASSWORD);
+//echo date("W");
+//mysql  SELECT WEEK(NOW(),3)
+$sql = "SELECT iddes FROM desempeno WHERE semana = ".date("W");
+$res = $db->query($sql);
+echo $db->numRows() . " Regisros encontrados.<br />Número de semana: ".date("W");
+if($db->numRows()==0)
+{
+	$ff = date("Y-m-d");
+	$fi = date('Y-m-d', strtotime('-7 days'));
+	$sql="SELECT cu.cobrador, count(pa.id) total 
+		FROM cuentas cu, pagos pa 
+		WHERE cu.id=pa.cuenta AND cu.estado=0  AND pa.estado<2
+		AND pa.fecha BETWEEN CAST('$fi' AS DATE) AND CAST('$ff' AS DATE)
+		GROUP BY cu.cobrador ORDER BY cu.cobrador";
+	echo $sql;
+	$res=$db->query($sql);
+	echo "<br />".$db->numRows()."<br />";
+	while($rd=$db->fetchNextObject($res))
+	{
+		#INFORMACION PRINCIPAL DE REPORTE DE DESEMPEÑO, VENDEDOR Y TOTAL COBRADO
+		$dcobra = $rd->cobrador;
+		$dtotal = $rd->total;
+		$totalavance = $rd->total;
+		#BUSCANDO COBROS EN FECHA
+		$sql="SELECT count(*) cobrosef
+		FROM cuentas cu, pagos pa
+		WHERE cu.id=pa.cuenta AND cu.estado=0 AND pa.estado=1 AND pa.fecha BETWEEN CAST('$fi' AS DATE) AND CAST('$ff' AS DATE)
+		AND cu.cobrador='".$rd->cobrador."'
+		AND pa.fechaPago<=pa.fecha";
+		$re2=$db->query($sql);
+		while($get=$db->fetchNextObject($re2))
+		{
+			$cobrosenfecha = $get->cobrosef;
+		}
+		#BUSCANDO COBROS FUERA DE FECHA
+		$sql="SELECT count(*) cobrosff
+		FROM cuentas cu, pagos pa
+		WHERE cu.id=pa.cuenta AND cu.estado=0 AND pa.estado=1 AND pa.fecha BETWEEN CAST('$fi' AS DATE) AND CAST('$ff' AS DATE)
+		AND cu.cobrador='".$rd->cobrador."'
+		AND pa.fechaPago>pa.fecha";
+		$re2=$db->query($sql);
+		while($get=$db->fetchNextObject($re2))
+		{
+			$cobrosfufecha = $get->cobrosff;
+		}
+		#BUSCANDO COBROS PENDIENTES
+		$sql="SELECT count(*) cobrospc
+		FROM cuentas cu, pagos pa
+		WHERE cu.id=pa.cuenta AND cu.estado=0 AND pa.estado=0 AND pa.fecha BETWEEN CAST('$fi' AS DATE) AND CAST('$ff' AS DATE)
+		AND cu.cobrador='".$rd->cobrador."'";
+		$re2=$db->query($sql);
+		while($get=$db->fetchNextObject($re2))
+		{
+			$cobrosporcobr = $get->cobrospc;
+		}		
+		$sql = "INSERT INTO desempeno (semana,cobrador,total, en_fecha, fuera_fecha, por_cobrar)
+			VALUES(".date("W").", '$dcobra', $dtotal, $cobrosenfecha, $cobrosfufecha, $cobrosporcobr)";
+		echo "<br />$sql<br />";
+		$db->execute($sql);
+	}
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////
+?>
