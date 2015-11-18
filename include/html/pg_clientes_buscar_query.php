6cbca693c79d0d4e064826a27ca024ed5f8e31f4
juanmarin <jmarincastro@gmail.com>
2015-11-18 10:36:07 -0700

    Correccion en el query de busqueda

--- include/html/pg_clientes_buscar_query.php
+++ include/html/pg_clientes_buscar_query.php
@@ -28,9 +28,21 @@ if($_SESSION["U_NIVEL"] == 0){
 } else {
 	$cobrador = "AND clientes.c_cobrador = '".$_SESSION["USERNAME"]."'";
 }
+if(isset($_POST["consulta"])){
 $sql = "SELECT * FROM clientes WHERE (nombre LIKE '%".$_POST["consulta"]."%' OR apellidop LIKE '%".$_POST["consulta"]."%' OR apellidom LIKE '%".$_POST["consulta"]."%') $cobrador";
 //echo $sql;
 $res = $db->query($sql);
+}elseif($_POST["consulta_cuenta"] != ""){
+	
+$sql = "SELECT cuentas.id, cuentas.cliente AS id, cuentas.estado, clientes.nombre, clientes.apellidop, clientes.apellidom
+FROM cuentas, clientes
+WHERE (cuentas.id = ".$_POST["consulta_cuenta"]." AND cuentas.cliente = clientes.id AND cuentas.estado = 0)";
+$res = $db->query($sql);
+$registros = $db->numRows($res);
+if ($registros == 0){
+	echo "N&uacute;mero de contrato no encontrado";	
+	}
+	}
 while ($ln = $db->fetchNextObject($res))
 {
 	?>
