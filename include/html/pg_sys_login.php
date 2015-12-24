df04007baccf951092ce626d029be49d763bdde8
juanmarin <jmarincastro@gmail.com>
2015-12-24 11:17:26 -0700

    Quitando leyenda del nuevo server al login

--- include/html/pg_sys_login.php
+++ include/html/pg_sys_login.php
@@ -7,14 +7,14 @@
   </head>
   <body>
     <div class="wrapper">
-	<div class="container">
-		<h1>Confianzp</h1>
-		<form class="form" action="" method="post">
-			<input type="text" name="uname" id="usuario" placeholder="Usuario">
-			<input type="password" name="pwd" placeholder="Contraseña">
-			<input type="submit" id="login-button" value="Entrar">
-		</form>
-	</div>
+		<div class="container">
+			<h1>Confianzp</h1>
+			<form class="form" action="" method="post">
+				<input type="text" name="uname" id="usuario" placeholder="Usuario">
+				<input type="password" name="pwd" placeholder="Contraseña">
+				<input type="submit" id="login-button" value="Entrar">
+			</form>
+		</div>
 	<ul class="bg-bubbles">
 		<li></li>
 		<li></li>
